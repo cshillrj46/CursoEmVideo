c52e1b8ee7a73f27672a49943e70918411e3a255
cshillrj46 <155317967+cshillrj46@users.noreply.github.com>
2024-01-10 20:01:12 -0300

    Create pratice05.py

---
+++ praticar/pratice05.py
@@ -0,0 +1,21 @@
+'''c = 1
+while c <= 10:
+    print(c)
+    c = c + 1'''
+
+'''n = 1
+while n != 0:
+    n = int(input('Digite um valor: '))
+print('FIM')'''
+
+n = 1
+par = 0
+impar = 0
+while n != 0:
+    n = int(input('Digite um número: '))
+    if n % 2 == 0:
+        par = par + 1
+    else:
+        impar = impar + 1
+print('{} números pares e {} números ímpares.'.format(par, impar))
+
