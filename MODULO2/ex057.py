0c62123f8920fde1cd5137b990cec5d77ddf728a
cshillrj46 <155317967+cshillrj46@users.noreply.github.com>
2024-01-12 15:06:39 -0300

    .

--- MODULO2/ex057.py
+++ MODULO2/ex057.py
@@ -1,6 +1,6 @@
 sexo = ''
 while sexo != 'M' and sexo != 'F':
-    sexo = str(input('Digite seu sexo [M/F]: ')).upper().strip()
+    sexo = str(input('Digite seu sexo [M/F]: ')).upper().strip()[0]
     if sexo != 'M' and sexo != 'F':
         print('OPÇÃO ERRADA')
 print('FIM')
